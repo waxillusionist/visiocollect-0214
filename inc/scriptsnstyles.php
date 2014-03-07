0070de4caf8893c7aeffaf52bb7c1f251178a028
Simon Lepel <simbo@simbo.de>
2014-03-08 00:06:49 +0100

    fix transitions

--- inc/scriptsnstyles.php
+++ inc/scriptsnstyles.php
@@ -71,11 +71,11 @@ add_action( 'wp_head', 'vc_fix_ltie9' );
    ========================================================================== */
 
 function vc_scripts_n_styles() {
-	wp_enqueue_style('vc-styles', THEME_URI.'assets/css/styles.min.css', array(), 'c39c');
+	wp_enqueue_style('vc-styles', THEME_URI.'assets/css/styles.min.css', array(), '0ccd');
 	wp_register_script('modernizr', THEME_URI.'assets/js/vendor/modernizr.min.js', array(), null, true);
 	wp_register_script('gmaps-api', 'https://maps.google.com/maps/api/js?sensor=false', array(), null, true);
 	$script_dependencies = array('modernizr','jquery','gmaps-api');
-	wp_register_script('vc-scripts', THEME_URI.'assets/js/scripts.min.js', $script_dependencies, '584c', true);
+	wp_register_script('vc-scripts', THEME_URI.'assets/js/scripts.min.js', $script_dependencies, '4bd4', true);
 	wp_enqueue_script('vc-scripts');
 }
 add_action( 'wp_enqueue_scripts', 'vc_scripts_n_styles', 99 );
