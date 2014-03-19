603b2d5ee01e867d9607a66a4d9851c94348461e
Simon Lepel <simbo@simbo.de>
2014-03-19 10:43:35 +0100

    prepare index template for blog grid

--- index.php
+++ index.php
@@ -12,18 +12,21 @@ if( have_posts() ) {
 
     ?>
     <div class="container">
-        <?php
-
-        while(have_posts()) {
-
-            the_post();
-            get_template_part( 'loop', 'index' );
-
-        }
-
-        vc_content_nav();
-
-        ?>
+        <div class="row blog-grid">
+            <?php
+            $i = 0;
+            while(have_posts()) {
+                echo '<div class="col col-sm-6 col-md-4">';
+                the_post();
+                get_template_part( 'loop', 'index' );
+                echo '</div>';
+                echo $i>0&&$i%2==0?'<div class="clear visible-sm"></div>':'';
+                echo $i>0&&$i%3==0?'<div class="clear visible-md"></div>':'';
+                $i++;
+            }
+            ?>
+        </div>
+        <?php vc_content_nav(); ?>
     </div>
     <?php
 
