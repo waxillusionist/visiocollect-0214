8533239d06365d72110322fc231e7b5f3f4e1978
Simon Lepel <simbo@simbo.de>
2014-03-20 16:35:20 +0100

    add next/prev post links

--- loop-post.php
+++ loop-post.php
@@ -20,3 +20,17 @@ $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post_id), 'large' )
         <?php the_content(); ?>
     </div>
 </article>
+<div class="pagination">
+    <?php
+        $next_post = get_next_post();
+        $next_url = $next_post ? get_permalink($next_post->ID) : false;
+        $next_tag = $next_post ? 'a' : 'span';
+        $next_title = $next_post ? esc_attr(strip_tags(apply_filters('the_title',$next_post->post_title))) : '';
+        $prev_post = get_adjacent_post();
+        $prev_url = $prev_post ? get_permalink($prev_post->ID) : false;
+        $prev_tag = $prev_post ? 'a' : 'span';
+        $prev_title = $prev_post ? esc_attr(strip_tags(apply_filters('the_title',$prev_post->post_title))) : '';
+        echo '<'.$prev_tag.( $prev_url ? ' href="'.$prev_url.'"' : '' ).' class="pagination-link next" title="'.$prev_title.'"><span class="glyphicon glyphicon-chevron-left"></span></'.$prev_tag.'>'.
+             '<'.$next_tag.( $next_url ? ' href="'.$next_url.'"' : '' ).' class="pagination-link prev" title="'.$next_title.'"><span class="glyphicon glyphicon-chevron-right"></span></'.$next_tag.'> ';
+    ?>
+</div>
